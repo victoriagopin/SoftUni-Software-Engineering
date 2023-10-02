eee612c2229da40d20d4d52826f4ed9de4ddef3a
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-02 14:17:24 +0300

    changed solution to exercise equalsums

--- JS-Fundamentals/Exercises/equalSums.js
+++ JS-Fundamentals/Exercises/equalSums.js
@@ -1,9 +1,23 @@
-function equalSums(arr) {
+function checkForEqualSums(arr) {
+    let result = 'no';
     for (let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        for (let j = 0; j < i; j++) {
+            leftSum += arr[j];
+        }
 
+        for (let k = i + 1; k < arr.length; k++) {
+            rightSum += arr[k];
+        }
 
-
+        if (leftSum === rightSum) {
+            result = i;
+            break;
+        }
     }
+
+    console.log(result);
 }
 
-equalSums([1, 2, 3, 3]);
+checkForEqualSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 3, 1, 4])
