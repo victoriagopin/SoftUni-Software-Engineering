341b2e6b745f72c609a097cf4b46a4e6036c4671
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-04 16:58:09 +0300

    started arrayModifier

---
+++ JS-Fundamentals/Exams/arrayModifier.js
@@ -0,0 +1,17 @@
+function arrayModifier(arr) {
+    let curArr = arr.shift();
+    let newArr = curArr.split(' ').map(Number);
+
+
+}
+
+arrayModifier([
+    '23 -2 321 87 42 90 -123',
+    'swap 1 3',
+    'swap 3 6',
+    'swap 1 0',
+    'multiply 1 2',
+    'multiply 2 1',
+    'decrease',
+    'end'
+])
