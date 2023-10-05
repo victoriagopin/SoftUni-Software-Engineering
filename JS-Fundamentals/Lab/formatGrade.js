ac10d29defa53ee540ad05d9ab8f0486d5f85097
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-05 10:45:39 +0300

    added new exercise

--- JS-Fundamentals/Lab/formatGrade.js
+++ JS-Fundamentals/Lab/formatGrade.js
@@ -15,9 +15,9 @@ function formatGrade(grade) {
         console.log(`Good (${grade.toFixed(2)})`);
     } else if (grade < 5.50) {
         console.log(`Very good (${grade.toFixed(2)})`);
-    } else {
-        console.log(`Ecxellent (${grade.toFixed(2)})`);
+    } else if (grade <= 6) {
+        console.log(`Excellent (${grade.toFixed(2)})`);
     }
 }
 
-formatGrade(3.33)
+formatGrade(7)
---
+++ JS-Fundamentals/Lab/mathPower.js
@@ -0,0 +1,8 @@
+// Write a function that calculates and print the value of a number raised to a given power:
+
+function mathPower(num, power) {
+    let result = Math.pow(num, power);
+    console.log(result);
+}
+
+mathPower(2, 8)
