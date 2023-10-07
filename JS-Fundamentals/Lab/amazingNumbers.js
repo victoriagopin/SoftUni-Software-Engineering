0fcf026b9e4d41ae68cfc8aa330f0623b6208d8f
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-07 15:35:13 +0300

    updated amazingNumbers

--- JS-Fundamentals/Lab/amazingNumbers.js
+++ JS-Fundamentals/Lab/amazingNumbers.js
@@ -1,3 +1,10 @@
+// Write a function, which as input will receive a number.
+// Check and print if it is amazing or not into the following format:
+// "{number} Amazing? {True or False}"
+// An amazing number includes the digit 9 the sum of its digits.
+// Examples for amazing numbers are 1233 (1 + 2 + 3 + 3 = 9), 
+// 583472 (5 + 8 + 3 + 4 + 7 + 2 = 29)
+
 function amazingNumbers(num) {
     let sum = 0;
     let numAsString = String(num);
