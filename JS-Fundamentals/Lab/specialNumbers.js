b46c3279e7088e6c04b4c9197f701820a48f5392
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-07 15:32:32 +0300

    updated specialNumbers

--- JS-Fundamentals/Lab/specialNumbers.js
+++ JS-Fundamentals/Lab/specialNumbers.js
@@ -1,3 +1,8 @@
+// Write a program that receives a number n. For all numbers in 
+// the range [1…n] print the number and if it is special or
+// not (True / False).
+// • A number is special when its sum of digits is 5, 7 or 11
+
 function specialNumbers(num) {
     let sum = 0;
 
