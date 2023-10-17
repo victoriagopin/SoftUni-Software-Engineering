6414e605e7ae1398b9f00448ab7cfd6dd922b58e
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-17 11:14:03 +0300

    updated heartDelivery.js

--- JS-Fundamentals/Exams/heartDelivery.js
+++ JS-Fundamentals/Exams/heartDelivery.js
@@ -1,20 +1,40 @@
-function heartDelivery(arr) {
-    let neighbourhood = arr.shift().split('@');
+function celebrateValentineDay(input) {
+    let neighborhood = input.shift().split('@').map(Number);
+    let jumpCommands = input.slice(0, input.indexOf('Love!'));
+    let position = 0;
 
-    for (let el of arr) {
-        let tokens = el.split(' ');
-        let command = tokens[0];
-        let jumpLength = Number(tokens[2]);
+    for (let i = 0; i < jumpCommands.length; i++) {
+        let command = jumpCommands[i].split(' ');
+        let length = Number(command[1]);
+        position = (position + length) % neighborhood.length;
 
-        if (command == 'Jump') {
-
+        if (neighborhood[position] === 0) {
+            console.log(`Place ${position} already had Valentine's day.`);
+        } else {
+            neighborhood[position] -= 2;
+            if (neighborhood[position] === 0) {
+                console.log(`Place ${position} has Valentine's day.`);
+            }
         }
-
     }
 
+    let notCelebrated = neighborhood.filter(x => x > 0).length;
+    if (notCelebrated === 0) {
+        console.log(`Mission was successful.`);
+    } else {
+        console.log(`Cupid has failed ${notCelebrated} places.`);
+    }
+    console.log(`Cupid's last position was ${position}.`);
 }
 
-heartDelivery(["10@10@10@2",
-    "Jump 1",
+celebrateValentineDay([
+    "2@4@2",
     "Jump 2",
-    "Love!"])
+    "Jump 2",
+    "Jump 8",
+    "Jump 3",
+    "Jump 1",
+    "Love!"
+]);
+
+
