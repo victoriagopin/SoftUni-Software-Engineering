d66851bfa3cbd05d8879446dfc54cd20a7d482f1
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-20 11:55:54 +0300

    Create shootForWin.js

---
+++ JS-Fundamentals/Exams/shootForWin.js
@@ -0,0 +1,45 @@
+// Write a program that helps you keep track of your shot targets. You will receive a sequence with integers, separated by a single space, representing targets and their value. Afterward, you will be receiving indices until the "End" command is given, and you need to print the targets and the count of shot targets.
+// Every time you receive an index, you need to shoot the target on that index, if it is possible. 
+// Every time you shoot a target, its value becomes -1, and it is considered a shot. Along with that, you also need to:
+// Reduce all the other targets, which have greater values than your current target, with its value. 
+// Increase all the other targets, which have less than or equal value to the shot target, with its value.
+// Keep in mind that you can't shoot a target, which is already shot. You also can't increase or reduce a target, which is considered a shot.
+// When you receive the "End" command, print the targets in their current state and the count of shot targets in the following format:
+// "Shot targets: {count} -> {target1} {target2}… {targetn}"
+// Input / Constraints
+// On the first line of input, you will receive a sequence of integers, separated by a single space – the targets sequence.
+// On the following lines, until the "End" command, you be receiving integers each on a single line – the index of the target to be shot.
+// Output
+// The format of the output is described above in the problem description.
+
+function shootForWin(arr) {
+    let targetSequence = arr.shift().split(' ').map(Number);
+    let shotTargets = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != 'End') {
+            let indx = Number(arr[i]);
+            if (indx >= 0 && indx < targetSequence.length) {
+                shotTargets++;
+                let removedItem = targetSequence.splice(indx, 1, -1);
+
+                for (let z = 0; z < targetSequence.length; z++) {
+                    let num = removedItem[0];
+                    if (targetSequence[z] > num && targetSequence[z] != -1) {
+                        targetSequence[z] -= num;
+                    } else if (targetSequence[z] <= num && targetSequence[z] != -1) {
+                        targetSequence[z] += num;
+                    }
+                }
+            }
+        }
+    }
+    console.log(`Shot targets: ${shotTargets} -> ${targetSequence.join(' ')}`);
+}
+
+shootForWin((["30 30 12 60 54 66",
+    "5",
+    "2",
+    "4",
+    "0",
+    "End"]));
