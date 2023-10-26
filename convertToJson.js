601fa8353e07e05e52b02016b42d3088cb9a74b5
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-26 18:16:32 +0300

    Created new lab exercise

---
+++ convertToJson.js
@@ -0,0 +1,12 @@
+// Write a function that receives a string in JSON format and converts it to an object.
+// Loop through all the keys and print them with their values in format: "{key}: {value}"
+
+function json(str) {
+    let obj = JSON.parse(str);
+    let keys = Object.keys(obj);
+    for (let key of keys) {
+        console.log(`${key}: ${obj[key]}`);
+    }
+}
+
+json('{"name": "George", "age": 40, "town": "Sofia"}');
