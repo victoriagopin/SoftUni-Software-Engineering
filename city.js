7ce9601269486340828e1024edfd592f759a6f3f
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-26 18:12:14 +0300

    Created city.js

---
+++ city.js
@@ -0,0 +1,18 @@
+// Write a function that receives a single parameter – an object, containing five properties:
+// { name, area, population, country, postcode }
+// Loop through all the keys and print them with their values in format: "{key} -> {value}"
+
+function city(obj) {
+    let keys = Object.keys(obj);
+    for (let key of keys) {
+        console.log(`${key} -> ${obj[key]}`);
+    }
+}
+
+city({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+});
