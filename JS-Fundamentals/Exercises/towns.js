ff079be829a588eb6c40d63cf179d8d73b6e4b5d
Victoria Gopin <victoriavgopin@gmail.com>
2023-10-30 18:19:58 +0200

    Added new exercise to Exercises

---
+++ JS-Fundamentals/Exercises/towns.js
@@ -0,0 +1,28 @@
+// You’re tasked to create and print objects from a text table.
+// You will receive the input as an array of strings, where each string represents a table row, with values on the row
+// separated by pipes " | " and spaces.
+// The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude
+// columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+// The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second
+// decimal point!
+
+function towns(arr) {
+
+    for (let town of arr) {
+        let tokens = town.split(' | ');
+        let curTown = tokens[0];
+        let latitude = Number(tokens[1]).toFixed(2);
+        let longitude = Number(tokens[2]).toFixed(2);
+
+        let city = {
+            town: curTown,
+            latitude: latitude,
+            longitude: longitude
+        }
+
+        console.log(city)
+    }
+}
+
+towns(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']);
