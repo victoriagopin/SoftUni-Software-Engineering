498e584a030e4d616b8b8f985dc874e5ec3455b6
Victoria Gopin <victoriavgopin@gmail.com>
2023-11-09 15:51:13 +0100

    Created printChars.js

---
+++ JS-Fundamentals/Lab/printChars.js
@@ -0,0 +1,9 @@
+// Write a function that receives a string and prints all the characters on separate lines.
+
+function printChars(word) {
+    for (let char of word) {
+        console.log(char);
+    }
+}
+
+printChars('AwORd');
