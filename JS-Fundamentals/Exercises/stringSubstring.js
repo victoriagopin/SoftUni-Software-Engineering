95994de1f3b0cb7067761af0cf8a2e4b73b9ee07
Victoria Gopin <victoriavgopin@gmail.com>
2023-11-11 13:54:44 +0100

    Created stringSubstring.js

---
+++ JS-Fundamentals/Exercises/stringSubstring.js
@@ -0,0 +1,18 @@
+// The input will be given as two separated strings(a word as a first parameter and a text as a second).
+// Write a function that checks given text for containing a given word.The comparison should be case insensitive.
+// Once you find a match, print the word and stop the program.
+// If you don't find the word print: "{word} not found!"
+
+function stringSubstring(word, text) {
+    text = text.toLowerCase();
+    let words = text.split(' ');
+
+    if (words.includes(word)) {
+        console.log(word);
+    } else {
+        console.log(`${word} not found!`);
+    }
+}
+
+stringSubstring('javascript',
+    'JavaScript is the best programming language');
