239388046c5d633c49b21037fb0517457015e29e
Victoria Gopin <victoriavgopin@gmail.com>
2023-11-14 14:08:09 +0100

    Created hardWord.js

---
+++ JS-Fundamentals/Exercises/hardWords.js
@@ -0,0 +1,37 @@
+// You will receive an array, which holds the string and another array.
+// The string is a letter from a young boy who does not yet know some words and you have to help him. The letter has
+// a few holes, these holes are the words unknown to the boy and you must fill them with strings from the array you
+// receive at the second index.
+// If the length of the hole is 4 you have to replace it with string with the same length and so on…
+
+function hardWords(arr) {
+    let sentences = arr.shift();
+    let words = arr.shift()
+    let textArr = sentences.split(' ');
+    let sloths = textArr.filter(word => word.includes('_') && word.length > 1);
+
+    for (let sloth of sloths) {
+        if (sloth[sloth.length - 1] == '_') {
+            for (let word of words) {
+                if (word.length == sloth.length) {
+                    sentences = sentences.replace(sloth, word);
+                    break;
+                }
+            }
+        } else {
+            let lastSymbol = sloth[sloth.length - 1];
+            let curSloth = sloth.slice(0, - 1);
+            for (let word of words) {
+                if (word.length == curSloth.length) {
+                    sentences = sentences.replace(sloth, word + lastSymbol);
+                    break;
+                }
+            }
+        }
+
+    }
+    console.log(sentences);
+}
+
+hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+    ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]);
