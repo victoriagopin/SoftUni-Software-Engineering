1475d6bffe861b9a128f825143ba8128bd7ef787
Victoria Gopin <victoriavgopin@gmail.com>
2023-11-25 12:51:56 +0100

    Started mirrorWords.js

---
+++ JS-Fundamentals/Exams/mirrorWords.js
@@ -0,0 +1,9 @@
+function mirrorWords([input]) {
+
+    let pattern = /(@|#)([A-Za-z]{2,})\1\1([A-Za-z]{2,})\1/g;
+
+}
+
+mirrorWords([
+    '@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'
+]);
