1845b38e66c923b0becbc640ed32f630fc37453c
Victoria Gopin <victoriavgopin@gmail.com>
2023-11-27 18:17:37 +0100

    Updated mirrorWords.js

--- JS-Fundamentals/Exams/mirrorWords.js
+++ JS-Fundamentals/Exams/mirrorWords.js
@@ -1,6 +1,63 @@
+/*
+On the first line of the input, you will be given a text string. To win the competition, you have to find all hidden word pairs, read them, and mark the ones that are mirror images of each other.
+First of all, you have to extract the hidden word pairs. Hidden word pairs are:
+Surrounded by "@" or "#" (only one of the two) in the following pattern #wordOne##wordTwo# or @wordOne@@wordTwo@
+At least 3 characters long each (without the surrounding symbols).
+Made up of letters only.
+If the second word, spelled backward, is the same as the first word and vice versa (casing matters!), they are a match, and you have to store them somewhere. Examples of mirror words: 
+#Part##traP# @leveL@@Level@ #sAw##wAs#
+If you don`t find any valid pairs, print: "No word pairs found!"
+If you find valid pairs print their count: "{valid pairs count} word pairs found!"
+If there are no mirror words, print: "No mirror words!"
+If there are mirror words print:
+"The mirror words are:
+{wordOne} <=> {wordtwo}, {wordOne} <=> {wordtwo}, … {wordOne} <=> {wordtwo}"
+Input / Constraints
+You will recive a string.
+Output
+Print the proper output messages in the proper cases as described in the problem description.
+If there are pairs of mirror words, print them in the end, each pair separated by ", ".
+Each pair of mirror word must be printed with " <=> " between the words.
+
+*/
+
 function mirrorWords([input]) {
 
-    let pattern = /(@|#)([A-Za-z]{2,})\1\1([A-Za-z]{2,})\1/g;
+    let pattern = /(@|#)([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1/gi;
+    let pairs = [];
+
+    let matches = pattern.exec(input);
+    let mirrorWords = 0;
+    let validPairs = 0;
+
+
+    while (matches) {
+        validPairs++;
+        let firstWord = matches[2];
+        let secondWord = matches[3];
+        let reversed = secondWord.split('').reverse().join('');
+
+        if (firstWord == reversed) {
+            mirrorWords++;
+            //let concat = firstWord + ' ' + '<=>' + ' ' + secondWord;
+            pairs.push(`${firstWord} <=> ${secondWord}`);
+        }
+
+        matches = pattern.exec(input);
+    }
+
+    if (validPairs > 0) {
+        console.log(`${validPairs} word pairs found!`);
+    } else {
+        console.log(`No word pairs found!`);
+    }
+
+    if (pairs.length == 0) {
+        console.log(`No mirror words!`);
+    } else {
+        console.log(`The mirror words are:`);
+        console.log(pairs.join(', '));
+    }
 
 }
 
