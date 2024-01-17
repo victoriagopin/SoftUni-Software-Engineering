c41e1e49f9cd49ce7dd6ce9669da4ea348bd9ce7
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-17 21:33:20 +0100

    Created day.js

---
+++ JS-Advanced/Lab/sumNums.js
@@ -0,0 +1,12 @@
+function sumNums(n1, n2) {
+    let start = Number(n1);
+    let end = Number(n2);
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        let curNum = i;
+        sum += curNum;
+    }
+    console.log(sum);
+}
+
+sumNums(-8, 20);
