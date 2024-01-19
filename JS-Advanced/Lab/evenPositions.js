e899ed2ef79d97e7fc14f4f3b54c2a1369c5cbcb
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-19 16:38:38 +0100

    Created evenPositions.js

---
+++ JS-Advanced/Lab/evenPositions.js
@@ -0,0 +1,10 @@
+function evenPositions(arr) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i += 2) {
+        result.push(arr[i]);
+    }
+
+    console.log(result.join(' '));
+}
+evenPositions(['20', '30', '40', '50', '60']);
