f855f770602b886d938095f4c13d18daae934c7a
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-19 17:34:09 +0100

    Created negAndPosNums.js

--- JS-Advanced/Lab/NegAndPosNums.js
+++ JS-Advanced/Lab/NegAndPosNums.js
@@ -1,4 +1,14 @@
-function negativeAndPositiveNums() {
+function negativeAndPositiveNums(arr) {
+    let modifiedArr = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            modifiedArr.unshift(arr[i]);
+        } else {
+            modifiedArr.push(arr[i]);
+        }
+    }
+    console.log(modifiedArr.join('\n'));
 }
 
+negativeAndPositiveNums([7, -2, 8, 9]);
---
+++ JS-Advanced/Lab/smallestTwoNums.js
@@ -0,0 +1,5 @@
+function smallestTwoNums() {
+
+}
+
+smallestTwoNums();
