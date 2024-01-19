5003ea51198e4a00d8ffb30f489d169df5d27f01
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-19 17:41:35 +0100

    Created smallestTwoNums.js

--- JS-Advanced/Lab/smallestTwoNums.js
+++ JS-Advanced/Lab/smallestTwoNums.js
@@ -1,5 +1,18 @@
-function smallestTwoNums() {
+function smallestTwoNums(arr) {
+    let twoMinNums = [];
+    let min = Math.min(...arr);
 
+    let indx = arr.indexOf(min);
+    let n1 = arr.splice(indx, 1);
+    twoMinNums.push(n1);
+
+
+    let min2 = Math.min(...arr);
+    let indx2 = arr.indexOf(min2);
+    let n2 = arr.splice(indx2, 1);
+    twoMinNums.push(n2);
+
+    console.log(twoMinNums.join(' '));
 }
 
-smallestTwoNums();
+smallestTwoNums([3, 0, 10, 4, 7, 3]);
