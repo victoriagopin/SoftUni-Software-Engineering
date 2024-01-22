729928e537052b161eec8cb2332319cffba0dae9
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-22 18:12:05 +0100

    Created cityRecord.js

---
+++ JS-Advanced/Lab/cityRecord.js
@@ -0,0 +1,13 @@
+function cityRecord(name, population, treasury) {
+    let record = {
+        name,
+        population,
+        treasury
+    }
+
+    return record
+}
+
+cityRecord('Santo Domingo',
+    12000,
+    23500);
