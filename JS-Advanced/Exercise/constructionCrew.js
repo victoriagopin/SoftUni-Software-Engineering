1851c15e0e9542c0b8f04960b7d8b83c36a08c34
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-23 16:47:48 +0100

    Created constructionCrew.js

---
+++ JS-Advanced/Exercise/constructionCrew.js
@@ -0,0 +1,14 @@
+function constructionCrew(obj) {
+    if (obj.dizziness) {
+        obj.levelOfHydrated += obj.weight * obj.experience * 0.1;
+        obj.dizziness = false;
+    }
+    return obj;
+}
+
+console.log(constructionCrew({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}))
