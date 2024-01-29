0e488e53041112362f06eef511e86c2452a9a46d
Victoria Gopin <victoriavgopin@gmail.com>
2024-01-29 23:57:46 +0100

    Uploading

--- JS-Advanced/Exercise/01. Subtraction/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    const n1 = document.getElementById('firstNumber');
-    const n2 = document.getElementById('secondNumber');
-
-    let result = Number(n1.value) - Number(n2.value);
-
-    document.getElementById('result').textContent = result;
-}
