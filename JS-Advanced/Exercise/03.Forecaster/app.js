f426bfc1c1401456b1760337ee2530f4d4cbb1ee
Victoria Gopin <victoriavgopin@gmail.com>
2024-02-28 12:46:25 +0100

    Finished Forecaster

--- JS-Advanced/Exercise/03.Forecaster/app.js
+++ JS-Advanced/Exercise/03.Forecaster/app.js
@@ -3,16 +3,22 @@ function attachEvents() {
     const submit = document.getElementById('submit');
     submit.addEventListener('click', getWeather);
     let current = document.getElementById('current');
+    let upcomingForecast = document.getElementById('upcoming');
     let forecast = document.getElementById('forecast');
     let allLocationsURL = 'http://localhost:3030/jsonstore/forecaster/locations';
     let todayURL = 'http://localhost:3030/jsonstore/forecaster/today/';
+    let threeDaysURL = 'http://localhost:3030/jsonstore/forecaster/upcoming/';
 
     async function getWeather() {
+        upcomingForecast.innerHTML = '';
+        current.innerHTML = '';
+
         let response = await fetch(allLocationsURL);
         let data = await response.json();
         let curLocation = data.find(x => x.name == input.value);
 
         showToday(curLocation.code);
+        showForThreeDays(curLocation.code);
     }
 
     async function showToday(code) {
@@ -61,6 +67,47 @@ function attachEvents() {
             case "Overcast": return "&#x2601"
             case "Degrees": return "&#176"
         }
+    }
+
+    async function showForThreeDays(code) {
+        let response = await fetch(threeDaysURL + code);
+        let data = await response.json();
+
+        let div = document.createElement('div');
+        div.classList.add('forecast-info');
+
+        let span1 = createSection(data.forecast[0]);
+        let span2 = createSection(data.forecast[1]);
+        let span3 = createSection(data.forecast[2]);
+
+        div.appendChild(span1);
+        div.appendChild(span2);
+        div.appendChild(span3);
+
+        upcomingForecast.appendChild(div);
+    }
+
+    function createSection(obj) {
+        let upcomingSpan = document.createElement('span');
+        upcomingSpan.classList.add('upcoming');
+
+        let conditionSymbol = document.createElement('span');
+        conditionSymbol.innerHTML = symbol(obj.condition);
+        conditionSymbol.classList.add('symbol');
+
+        let degrees = document.createElement('span');
+        degrees.textContent = `${obj.low}°/${obj.high}°`;
+        degrees.classList.add('forecast-data');
+
+        let weather = document.createElement('span');
+        weather.textContent = obj.condition;
+        weather.classList.add('forecast-data');
+
+        upcomingSpan.appendChild(conditionSymbol);
+        upcomingSpan.appendChild(degrees);
+        upcomingSpan.appendChild(weather);
+
+        return upcomingSpan;
     }
 }
 
